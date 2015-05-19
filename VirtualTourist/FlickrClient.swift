f1b1ec186f246065cb06ba391c1c6363a0f2cf93
James P. Gilchrist <james.p.gilchrist@gmail.com>
2015-05-19 08:54:39 -0400

    carthage alamofire

---
+++ VirtualTourist/FlickrClient.swift
@@ -0,0 +1,21 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by James Gilchrist on 5/19/15.
+//  Copyright (c) 2015 James Gilchrist. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum FlickrClient /*: URLRequestConvertible */ {
+    
+    static let baseURLString = "https://api.flickr.com/services/rest/";
+
+    //https://www.flickr.com/services/api/flickr.photos.search.html
+    
+    //https://www.flickr.com/services/api/misc.urls.html
+    
+    
+}
